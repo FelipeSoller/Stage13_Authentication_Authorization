5630483772f394ed1c3f8b81a30d37144a5c186a
Felipe Soller <soller@outlook.com>
2024-03-05 14:13:51 -0300

    refactor to show pages according to the roles

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,18 +1,28 @@
 import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
 
+import { USER_ROLE } from '../../utils/roles';
+import { useAuth } from "../../hooks/auth";
+
 import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
 
 export function Home() {
+  const { user } = useAuth();
+  const isAdminOrSeller = [USER_ROLE.ADMIN, USER_ROLE.SELLER].includes(user.role);
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+        {isAdminOrSeller && (
+          <>
+            {user.role === USER_ROLE.ADMIN && <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />}
+            <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+          </>
+        )}
       </main>
     </Container>
   )
